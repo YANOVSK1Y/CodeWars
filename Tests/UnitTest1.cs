54acc95f30b0940553a8769977ea039f176ef357
Andrey <andreyianovskyi78@gmail.com>
2022-08-05 12:18:21 +0300

    Create main structure.

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using Solutions; 
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
