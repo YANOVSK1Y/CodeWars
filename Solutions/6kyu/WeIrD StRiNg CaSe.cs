3fe2cdb778560aec50f4c0fc07ac2a301c5fa9f1
Andrey <andreyianovskyi78@gmail.com>
2022-08-08 09:26:55 +0300

    WeIrD_StRiNg_CaSe Done.

---
+++ Solutions/6kyu/WeIrD StRiNg CaSe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; 
+
+namespace Solutions._6kyu
+{
+    public static class WeIrD_StRiNg_CaSe
+    {
+        /// <summary>
+        /// Write a function toWeirdCase (weirdcase in Ruby) that 
+        /// accepts a string, and returns the same string with all 
+        /// even indexed characters in each word upper cased, and 
+        /// all odd indexed characters in each word lower cased. 
+        /// The indexing just explained is zero based, so the 
+        /// zero-ith index is even, therefore that character should 
+        /// be upper cased and you need to start over for each word.
+        ///
+        ///The passed in string will only consist of alphabetical 
+        ///characters and spaces(' '). Spaces will only be present
+        ///if there are multiple words.Words will be separated by a single space(' ').
+        /// </summary>
+        public static string ToWeirdCase(string s)
+        {
+            var words = s.Split(' ');
+            ArrayList res = new ArrayList(); 
+            foreach (var item in words)
+            {
+                string word = string.Empty; 
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (i % 2 == 0) word += char.ToUpper(item[i]);
+                    else word += char.ToLower(item[i]); 
+                }
+                res.Add(word); 
+            }
+            
+            return string.Join(" ", res.ToArray()); 
+        }
+    }
+}
---
+++ Tests/6kyu/WeIrD StRiNg CaSe_test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Solutions._6kyu;
+
+namespace Tests._6kyu
+{
+    [TestFixture]
+    public class WeIrD_StRiNg_CaSe_Test
+    {
+        [Test]
+        public static void ShouldWorkForSomeExamples()
+        {
+            Assert.AreEqual("ThIs", WeIrD_StRiNg_CaSe.ToWeirdCase("This"));
+            Assert.AreEqual("Is", WeIrD_StRiNg_CaSe.ToWeirdCase("is"));
+            Assert.AreEqual("ThIs Is A TeSt", WeIrD_StRiNg_CaSe.ToWeirdCase("This is a test"));
+        }
+    }
+}
+
