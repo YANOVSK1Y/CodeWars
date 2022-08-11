c29a27b51d36f4b0283cf60fb3bfb99fd28aaa4a
Andrey <andreyianovskyi78@gmail.com>
2022-08-11 16:56:56 +0300

    Integers_Recreation_One DOne.

---
+++ Solutions/6kyu/Integers Recreation One.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Solutions._6kyu
+{
+    public static class Integers_Recreation_One
+    {
+        public static string listSquared(long m, long n)
+        {
+            var result = new List<string>();
+
+            for (long number = m; number <= n; number++)
+            {
+                var divisors = new List<int>();
+                long sum = 0;
+                for (int div = 1; div <= number; div++)
+                {
+                    if (number % div == 0)
+                        sum += (div * div);
+                }
+
+                if (Math.Sqrt(sum) % 2 == 0 || sum == 1)
+                    result.Add(string.Format("[{0}, {1}]", number, sum));
+            }
+            return string.Format("[{0}]", string.Join(", ", result));
+        }
+    }
+}
---
+++ Tests/6kyu/Integers Recreation One_Test.cs
@@ -0,0 +1,27 @@
+﻿
+using NUnit.Framework;
+using Solutions._6kyu; 
+
+namespace Tests._6kyu
+{
+    [TestFixture]
+    public class Integers_Recreation_One_Test
+    {
+
+        [Test]
+        public void Test01()
+        {
+            Assert.AreEqual("[[1, 1], [42, 2500], [246, 84100]]", Integers_Recreation_One.listSquared(1, 250));
+        }
+        [Test]
+        public void Test02()
+        {
+            Assert.AreEqual("[[42, 2500], [246, 84100]]", Integers_Recreation_One.listSquared(42, 250));
+        }
+        [Test]
+        public void Test03()
+        {
+            Assert.AreEqual("[[287, 84100]]", Integers_Recreation_One.listSquared(250, 500));
+        }
+    } 
+}
