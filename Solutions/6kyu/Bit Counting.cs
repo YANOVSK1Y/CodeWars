4800ffdd6eb9daaa7ad270de9fce9420f8ed1848
Andrey <andreyianovskyi78@gmail.com>
2022-08-14 21:31:16 +0300

    Bit_Counting Done.

---
+++ Solutions/6kyu/Bit Counting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Solutions._6kyu
+{
+    public static class Bit_Counting
+    {
+        public static int CountBits(int n)
+        {
+            return Convert.ToString(Convert.ToString(n, 2)).Where(item => item.Equals('1')).Count(); 
+        }
+    }
+}
---
+++ Tests/6kyu/Bit Counting_Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Solutions._6kyu;
+
+namespace Tests._6kyu
+{
+    public class Bit_Counting_Test
+    {
+        [Test]
+        public void TestCase_Bit_Counting()
+        {
+            Assert.AreEqual(5, Bit_Counting.CountBits(1234)); 
+        }
+    }
+}
