7732f326721f776c6522ab29a0808a8d0290b860
Andrey <andreyianovskyi78@gmail.com>
2022-08-15 20:11:54 +0300

    Strip_Comments DOne.

---
+++ Solutions/4kyu/Strip Comments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._4kyu
+{
+    /// <summary>
+    /// Complete the solution so that it strips 
+    /// all text that follows any of a set of comment 
+    /// markers passed in. Any whitespace at the end 
+    /// of the line should also be stripped out. 
+    /// </summary>
+    public static class Strip_Comments
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            string[] st = text.Split('\n');
+            var res = st.Select(item => item.Split(commentSymbols, StringSplitOptions.None).First().TrimEnd()).ToArray();
+            return String.Join("\n", res);
+        }
+    }
+}
---
+++ Tests/4kyu/Strip Comments_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using Solutions._4kyu;
+using Solutions._4kyu;
+
+namespace Tests._4kyu
+{
+    [TestFixture]
+    public class Strip_Comments_Test
+    {
+        [Test]
+        public void StripComments()
+        {
+            Assert.AreEqual(
+                    "apples, pears\ngrapes\nbananas",
+                    Strip_Comments.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new string[] { "#", "!" }));
+
+            Assert.AreEqual("a\nc\nd", Strip_Comments.StripComments("a #b\nc\nd $e f g", new string[] { "#", "$" }));
+
+        }
+    }
+}
