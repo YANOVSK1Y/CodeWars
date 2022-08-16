c99bb0bd9389464ea49aff1a18af788baf6144c9
Andrey <andreyianovskyi78@gmail.com>
2022-08-16 12:59:46 +0300

    Path_Finder_1_Сan_you_reach_the_exit DOne

---
+++ Solutions/4kyu/Path Finder 1 Сan you reach the exit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Solutions._4kyu
+{
+    public static class Path_Finder_1_Сan_you_reach_the_exit
+    {
+        private static bool PathFinder(int[][] maze, int x = 0, int y = 0) =>
+        (x >= 0 && x < maze[0].Length) &&
+        (y >= 0 && y < maze.Length) &&
+        (maze[y][x] == 0) && (
+        (x + 1 == maze[0].Length && y + 1 == maze.Length) ||
+        (maze[y][x] = -1) == -1 && (
+            PathFinder(maze, x + 1, y) ||
+            PathFinder(maze, x - 1, y) ||
+            PathFinder(maze, x, y + 1) ||
+            PathFinder(maze, x, y - 1)));
+
+        public static bool PathFinder(string maze) =>
+            PathFinder(maze.Split('\n').Select(
+                line => line.Select(c => '.' - c).ToArray()
+            ).ToArray());
+        
+    }
+}
---
+++ Tests/4kyu/Path Finder 1 Сan you reach the exit_Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Solutions._4kyu;
+
+namespace Tests._4kyu
+{
+    [TestFixture]
+    public class Path_Finder_1_Сan_you_reach_the_exit_Test
+    {
+
+        [Test]
+        public void sampleTests()
+        {
+
+            string a = ".W.\n" +
+                       ".W.\n" +
+                       "...",
+
+                   b = ".W.\n" +
+                       ".W.\n" +
+                       "W..",
+
+                   c = "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......",
+
+                   d = "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       ".....W\n" +
+                       "....W.";
+
+            Assert.AreEqual(true, Path_Finder_1_Сan_you_reach_the_exit.PathFinder(a));
+            Assert.AreEqual(false, Path_Finder_1_Сan_you_reach_the_exit.PathFinder(b));
+            Assert.AreEqual(true, Path_Finder_1_Сan_you_reach_the_exit.PathFinder(c));
+            Assert.AreEqual(false, Path_Finder_1_Сan_you_reach_the_exit.PathFinder(d));
+        }
+    }
+}
