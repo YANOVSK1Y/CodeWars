b2d6ae89ccc946bfb44d1419fc5e26535fca4e6b
Andrey <andreyianovskyi78@gmail.com>
2022-08-19 09:56:21 +0300

    Next_bigger_number_with_the_same_digits Done

---
+++ Solutions/4kyu/Next bigger number with the same digits.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._4kyu
+{
+    public static class Next_bigger_number_with_the_same_digits
+    {
+        /// <summary>
+        /// Create a function that takes a positive integer and returns the next
+        /// bigger number that can be formed by rearranging its digits. For example:
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static long NextBiggerNumber(long n)
+        {
+            String str = GetNumbers(n);
+            for (long i = n + 1; i <= long.Parse(str); i++)
+            {
+                if (GetNumbers(n) == GetNumbers(i))
+                {
+                    return i;
+                }
+            }
+            return -1;
+            
+        }
+        public static string GetNumbers(long number)
+        {
+            return string.Join("", number.ToString().ToCharArray().OrderByDescending(x => x));
+        }
+    }
+}
---
+++ Tests/4kyu/Next bigger number with the same digits_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Solutions._4kyu;
+
+namespace Tests._4kyu
+{
+    [TestFixture]
+    public class Next_bigger_number_with_the_same_digits_Test
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("****** Small Number");
+            Assert.AreEqual(21, Next_bigger_number_with_the_same_digits.NextBiggerNumber(12));
+            Assert.AreEqual(531, Next_bigger_number_with_the_same_digits.NextBiggerNumber(513));
+            Assert.AreEqual(2071, Next_bigger_number_with_the_same_digits.NextBiggerNumber(2017));
+            Assert.AreEqual(441, Next_bigger_number_with_the_same_digits.NextBiggerNumber(414));
+            Assert.AreEqual(414, Next_bigger_number_with_the_same_digits.NextBiggerNumber(144));
+        }
+    }
+}
