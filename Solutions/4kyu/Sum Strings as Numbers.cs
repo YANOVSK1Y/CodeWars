8a94a898f90c3fb42b3d4eb57988b8f9743483fd
Andrey <andreyianovskyi78@gmail.com>
2022-08-19 10:36:57 +0300

    Sum_Strings_as_Numbers Done

---
+++ Solutions/4kyu/Sum Strings as Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._4kyu
+{
+    public static class Sum_Strings_as_Numbers
+    {
+        /// <summary>
+        /// Given the string representations of two integers, return 
+        /// the string representation of the sum of those integers.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static string sumStrings(string a, string b)
+        {
+            BigInteger.TryParse(a, out BigInteger a1);
+            BigInteger.TryParse(b, out BigInteger b1);
+            return Convert.ToString(a1 + b1);
+        }
+    }
+}
---
+++ Tests/4kyu/Sum Strings as Numbers_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Solutions._4kyu; 
+
+namespace Tests._4kyu
+{
+    public  class Sum_Strings_as_Numbers_Test
+    {
+        [Test]
+        public void Given123And456Returns579()
+        {
+            Assert.AreEqual("579", Sum_Strings_as_Numbers.sumStrings("123", "456"));
+        }
+    }
+}
