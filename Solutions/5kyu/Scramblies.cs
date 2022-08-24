cc1556109b7ed26d593b90d6d0f903479c16ca71
Andrey <andreyianovskyi78@gmail.com>
2022-08-24 18:15:40 +0300

    Scramblies done

---
+++ Solutions/5kyu/Scramblies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{   /// <summary>
+    /// Complete the function scramble(str1, str2) that returns true if a
+    /// portion of str1 characters can be rearranged to match str2, otherwise returns false.
+    /// 
+    ///Notes:
+    ///Only lower case letters will be used(a-z). No punctuation or digits will be included.
+    ///Performance needs to be considered.
+    /// </summary>
+    public static class Scramblies
+    {
+        public static bool Scramble(string str1, string str2)
+        {
+            return str2.All(x => str1.Count(y => y == x) >= str2.Count(y => y == x));
+        }
+
+    }
+}
---
+++ Tests/5kyu/Scramblies_Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class Scramblies_Test
+    {
+        private static void testing(bool actual, bool expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            testing(Scramblies.Scramble("rkqodlw", "world"), true);
+            testing(Scramblies.Scramble("cedewaraaossoqqyt", "codewars"), true);
+            testing(Scramblies.Scramble("katas", "steak"), false);
+            testing(Scramblies.Scramble("scriptjavx", "javascript"), false);
+            testing(Scramblies.Scramble("scriptingjava", "javascript"), true);
+            testing(Scramblies.Scramble("scriptsjava", "javascripts"), true);
+            testing(Scramblies.Scramble("javscripts", "javascript"), false);
+            testing(Scramblies.Scramble("aabbcamaomsccdd", "commas"), true);
+            testing(Scramblies.Scramble("commas", "commas"), true);
+            testing(Scramblies.Scramble("sammoc", "commas"), true);
+        }
+    }
+}
