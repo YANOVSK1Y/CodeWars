7526ac69452f62b3d1e9504dc0912e8446ab8bea
Andrey <andreyianovskyi78@gmail.com>
2022-08-26 09:59:43 +0300

    Where_my_anagrams_at Done

---
+++ Solutions/5kyu/Where my anagrams at.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public static class Where_my_anagrams_at
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            return words?.Where(w => w.OrderBy(c => c).SequenceEqual(word.OrderBy(c => c))).ToList();
+        }
+    }
+}
---
+++ Tests/5kyu/Where my anagrams at_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    [TestFixture]
+    public class Where_my_anagrams_at_Test
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new List<string> { "carer", "arcre", "carre" }, Where_my_anagrams_at.Anagrams("racer", new List<string> { "carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr" }));
+            Assert.AreEqual(new List<string> { "a" }, Where_my_anagrams_at.Anagrams("a", new List<string> { "a", "b", "c", "d" }));
+
+        }
+
+    }
+}
