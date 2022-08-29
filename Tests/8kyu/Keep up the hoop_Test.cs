ff0cca24710382163fe5e418db3113e6af74bcd4
Andrey <andreyianovskyi78@gmail.com>
2022-08-29 16:52:25 +0300

    Keep_up_the_hoop Done.

---
+++ Tests/8kyu/Keep up the hoop_Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using Solutions._8kyu; 
+
+namespace Tests._8kyu
+{
+    [TestFixture]
+    public class Keep_up_the_hoop_Test
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            Assert.AreEqual("Keep at it until you get it", Keep_up_the_hoop.HoopCount(6), "Should work for 6");
+            Assert.AreEqual("Great, now move on to tricks", Keep_up_the_hoop.HoopCount(22), "Should work for 22");
+        }
+    }
+}
