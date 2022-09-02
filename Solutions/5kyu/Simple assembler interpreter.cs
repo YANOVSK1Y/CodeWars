9594ef1b296a8bfee1b4b04c5d32cb22b91304d8
Andrey <andreyianovskyi78@gmail.com>
2022-09-02 14:20:02 +0300

    Simple_assembler_interpreter Done

---
+++ Solutions/5kyu/Simple assembler interpreter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class Simple_assembler_interpreter
+    {
+        public static Dictionary<string, int> Interpret(string[] program)
+        {
+            var memory = new Dictionary<string, int>();
+            int GetValue(string s) => int.TryParse(s, out var tmp) ? tmp : memory[s];
+            for (var pointer = 0; pointer < program.Length; pointer++)
+            {
+                var values = program[pointer].Split();
+                var _ = values[0] switch
+                {
+                    "mov" => memory[values[1]] = GetValue(values[2]),
+                    "inc" => memory[values[1]]++,
+                    "dec" => memory[values[1]]--,
+                    "jnz" => pointer += GetValue(values[1]) != 0 ? GetValue(values[2]) - 1 : 0,
+                    _ => throw new Exception("Input error")
+                };
+            }
+
+            return memory;
+        }
+    }
+}
---
+++ Tests/5kyu/Simple assembler interpreter_Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions._5kyu;
+using NUnit.Framework;
+
+namespace Tests._5kyu
+{
+    public class Simple_assembler_interpreter_Test
+    {
+        private void Test(Dictionary<string, int> expected, Dictionary<string, int> actual)
+        {
+            var expectedStr = "{ " +
+                              string.Join(", ", expected.Select(kv => $"{kv.Key}: {kv.Value}"))
+                              + " }";
+            var actualStr = "{ " +
+                            string.Join(", ", actual.Select(kv => $"{kv.Key}: {kv.Value}"))
+                            + " }";
+
+            Assert.AreEqual(expected, actual, $"Expected: {expectedStr}, Actual: {actualStr}");
+        }
+
+        [Test, Description("Should work for some example programs")]
+        public void ExamplePrograms()
+        {
+            Test(new Dictionary<string, int> { { "a", 1 } },
+                Simple_assembler_interpreter.Interpret(new[] { "mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a" }));
+            Test(new Dictionary<string, int> { { "a", 0 }, { "b", -20 } },
+                Simple_assembler_interpreter.Interpret(new[] { "mov a -10", "mov b a", "inc a", "dec b", "jnz a -2" }));
+        }
+    }
+}
