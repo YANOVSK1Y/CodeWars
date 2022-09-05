9f2f517f650b1eb9153f469603c425ca68e183a0
Andrey <andreyianovskyi78@gmail.com>
2022-09-05 16:23:56 +0300

    Adding_Big_Numbers Done.

---
+++ Solutions/4kyu/Adding Big Numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._4kyu
+{
+    public class Adding_Big_Numbers
+    {
+        ///<summary>
+        ///We need to sum big numbers and we require your help.
+        ///Write a function that returns the sum of two numbers.The input numbers
+        ///are strings and the function must return a string.
+        /// </summary>
+        public static string Add(string a, string b)
+        {
+            return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+        }
+    }
+}
---
+++ Tests/4kyu/Adding Big Numbers_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Solutions._4kyu;
+
+namespace Tests._4kyu
+{
+    public class Adding_Big_Numbers_Test
+    {
+        [Test]
+        public void ASimpleKataTest()
+        {
+            Assert.AreEqual("110", Adding_Big_Numbers.Add("91", "19"));
+            Assert.AreEqual("1111111111", Adding_Big_Numbers.Add("123456789", "987654322"));
+            Assert.AreEqual("1000000000", Adding_Big_Numbers.Add("999999999", "1"));
+        }
+    }
+}
