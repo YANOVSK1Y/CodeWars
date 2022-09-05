2e516d640403c6c1127b7b44c5d247f69a954059
Andrey <andreyianovskyi78@gmail.com>
2022-09-05 16:36:28 +0300

    Convert_PascalCase_string_into_snake_case Done.

---
+++ Solutions/5kyu/Convert PascalCase string into snake_case.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class Convert_PascalCase_string_into_snake_case
+    {
+        public static string ToUnderscore(int str)
+        {
+            return str.ToString();
+        }
+
+        public static string ToUnderscore(string str)
+        {
+            String res = String.Empty;
+
+            foreach(var item in str)
+            {
+                if (Char.IsLetter(item) && Char.IsUpper(item))
+                {
+                    if (res.Length != 0) res += "_" + Char.ToLower(item);
+                    else res += Char.ToLower(item);
+                }
+                else
+                {
+                    res += item;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tests/5kyu/Convert PascalCase string into snake_case_Test.cs
@@ -0,0 +1,29 @@
+﻿using Solutions._5kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests._5kyu
+{
+    [TestFixture]
+    public class Convert_PascalCase_string_into_snake_case_Test
+    {
+
+        [TestFixture]
+        public class Tests
+        {
+            [Test]
+            public void SampleTests()
+            {
+                Assert.AreEqual("test_controller", Convert_PascalCase_string_into_snake_case.ToUnderscore("TestController"));
+                Assert.AreEqual("this_is_beautiful_day", Convert_PascalCase_string_into_snake_case.ToUnderscore("ThisIsBeautifulDay"));
+                Assert.AreEqual("am7_days", Convert_PascalCase_string_into_snake_case.ToUnderscore("Am7Days"));
+                Assert.AreEqual("my3_code_is4_times_better", Convert_PascalCase_string_into_snake_case.ToUnderscore("My3CodeIs4TimesBetter"));
+                Assert.AreEqual("arbitrarily_sending_different_types_to_functions_makes_katas_cool", Convert_PascalCase_string_into_snake_case.ToUnderscore("ArbitrarilySendingDifferentTypesToFunctionsMakesKatasCool"));
+                Assert.AreEqual("1", Convert_PascalCase_string_into_snake_case.ToUnderscore(1), "Numbers should be turned to strings!");
+            }
+        }
+    }
+}
