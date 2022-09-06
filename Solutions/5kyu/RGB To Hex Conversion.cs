d6ada3e0c778d7088c679e8e055e8113e4aa2172
Andrey <andreyianovskyi78@gmail.com>
2022-09-06 09:35:33 +0300

    RGB_To_Hex_Conversion Done

---
+++ Solutions/5kyu/RGB To Hex Conversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class RGB_To_Hex_Conversion
+    {
+        public static string Rgb(int r, int g, int b)
+        {
+            if (r > 255) r = 255;
+            else if (r < 0) r = 0;
+            if (g > 255) g = 255;
+            else if (g < 0) g = 0;
+            if (b > 255) b = 255;
+            else if (b < 0) b = 0;
+            return string.Format("{0:X2}{1:X2}{2:X2}", r, g, b);
+        }
+    }
+}
---
+++ Tests/5kyu/RGB To Hex Conversion_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions._5kyu;
+
+namespace Tests._5kyu
+{
+    public class RGB_To_Hex_Conversion_Test
+    {
+        [Test]
+        public void FixedTests()
+        {
+            Assert.AreEqual("FFFFFF", RGB_To_Hex_Conversion.Rgb(255, 255, 255));
+            Assert.AreEqual("FFFFFF", RGB_To_Hex_Conversion.Rgb(255, 255, 300));
+            Assert.AreEqual("000000", RGB_To_Hex_Conversion.Rgb(0, 0, 0));
+            Assert.AreEqual("9400D3", RGB_To_Hex_Conversion.Rgb(148, 0, 211));
+            Assert.AreEqual("9400D3", RGB_To_Hex_Conversion.Rgb(148, -20, 211), "Handle negative numbers.");
+            Assert.AreEqual("90C3D4", RGB_To_Hex_Conversion.Rgb(144, 195, 212));
+            Assert.AreEqual("D4350C", RGB_To_Hex_Conversion.Rgb(212, 53, 12), "Consider single hex digit numbers.");
+        }
+    }
+}
+
