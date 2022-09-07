2a1b55910c1c2b002aceb27da9ecee19d01f1a0b
Andrey <andreyianovskyi78@gmail.com>
2022-09-07 09:39:36 +0300

    Mean_Square_Error Done.

---
+++ Solutions/5kyu/Mean Square Error.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class Mean_Square_Error
+    {
+        public static double Solution(int[] firstArray, int[] secondArray)
+        {
+            double res = 0; 
+            for (int i=0; i<Math.Min(firstArray.Length, secondArray.Length); i++)
+            {
+                res += Math.Pow(firstArray[i] - secondArray[i], 2); 
+
+            }
+            return res / Math.Min(firstArray.Length, secondArray.Length); 
+        }
+    }
+}
---
+++ Tests/5kyu/Mean Square Error_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class Mean_Square_Error_Test
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(9, Mean_Square_Error.Solution(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
+            Assert.AreEqual(16.5, Mean_Square_Error.Solution(new int[] { 10, 20, 10, 2 }, new int[] { 10, 25, 5, -2 }));
+            Assert.AreEqual(1, Mean_Square_Error.Solution(new int[] { 0, -1 }, new int[] { -1, 0 }));
+        }
+    }
+}
