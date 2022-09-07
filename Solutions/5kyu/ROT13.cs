ce78fcc87915fc5f53b99365e2dd6dc4e4a7862d
Andrey <andreyianovskyi78@gmail.com>
2022-09-07 09:59:48 +0300

    ROT13 DOne

---
+++ Solutions/5kyu/ROT13.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class ROT13
+    {
+        public static string Rot13(string input)
+        {
+            return string.Concat(input.Select(c => char.IsLetter(c) ? (char)((int)c + (char.ToLower(c) <= 'm' ? 13 : -13)) : c));
+        }
+
+    }
+}
---
+++ Tests/5kyu/ROT13_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class ROT13_Test
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("ROT13 example.", ROT13.Rot13("EBG13 rknzcyr."));
+        }
+    }
+}
