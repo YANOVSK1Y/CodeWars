49e13ec47627881982cd2b74b85894bc79db2808
Andrey <andreyianovskyi78@gmail.com>
2022-09-12 15:44:16 +0300

    Going_to_zero_or_to_infinity DOne.

---
+++ Solutions/5kyu/Going to zero or to infinity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class Going_to_zero_or_to_infinity
+    {
+        public static double going(int n)
+        {
+            double running = 1.0, term = 1.0;
+            for (int i = n; i > 1; i--) running += (term *= 1.0 / i);
+            return System.Math.Truncate(running * 1000000) / 1000000;
+        }
+    }
+}
---
+++ Tests/5kyu/Going to zero or to infinity_Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class Going_to_zero_or_to_infinity_Test
+    {
+        [Test]
+        public void Test01()
+        {
+            Assert.AreEqual(1.275, Going_to_zero_or_to_infinity.going(5));
+        }
+        [Test]
+        public void Test02()
+        {
+            Assert.AreEqual(1.2125, Going_to_zero_or_to_infinity.going(6));
+        }
+        [Test]
+        public void Test03()
+        {
+            Assert.AreEqual(1.173214, Going_to_zero_or_to_infinity.going(7));
+        }
+    }
+}
