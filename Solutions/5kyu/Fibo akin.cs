6cade62e9e944441cb5c3de65001910f99734df4
Andrey <andreyianovskyi78@gmail.com>
2022-09-13 14:45:55 +0300

    Fibo_akin Done

---
+++ Solutions/5kyu/Fibo akin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class Fibo_akin
+    {
+        public static long LengthSupUK(int n, int k)
+        {
+            return Sequence(n).Count(x => x >= k);
+        }
+
+        public static long Comp(int n)
+        {
+            var seq = Sequence(n);
+            return seq.Skip(1).Zip(seq, (a, b) => (a, b)).Count(t => t.Item1 < t.Item2);
+        }
+
+        private static IEnumerable<long> Sequence(int n)
+        {
+            var seq = new long[n + 1];
+            seq[0] = 0L;
+            seq[1] = 1L;
+            seq[2] = 1L;
+            for (var i = 3; i <= n; i++)
+                seq[i] = seq[i - seq[i - 1]] + seq[i - seq[i - 2]];
+            return seq;
+        }
+    }
+}
---
+++ Tests/5kyu/Fibo akin_Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions._5kyu;
+
+namespace Tests._5kyu
+{
+    public class Fibo_akin_Test
+    {
+        private static void dotest1(int n, int k, int expected)
+        {
+            Assert.AreEqual(expected, Fibo_akin.LengthSupUK(n, k));
+        }
+        private static void dotest2(int n, long expected)
+        {
+            Assert.AreEqual(expected, Fibo_akin.Comp(n));
+        }
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Basic Tests lengthSupUK");
+            dotest1(50, 25, 2);
+            dotest1(3332, 973, 1391);
+            dotest1(2941, 862, 1246);
+            dotest1(3177, 573, 2047);
+            dotest1(1745, 645, 474);
+        }
+        [Test]
+        public static void test2()
+        {
+            Console.WriteLine("Basic Tests comp");
+            dotest2(74626, 37128);
+            dotest2(71749, 35692);
+            dotest2(56890, 28281);
+            dotest2(60441, 30054);
+            dotest2(21361, 10581);
+        }
+    }
+}
