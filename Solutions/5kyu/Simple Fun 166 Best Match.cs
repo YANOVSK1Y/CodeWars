55379302c760e96d256e95fc6912ee5a0e40ba6c
Andrey <andreyianovskyi78@gmail.com>
2022-09-15 19:47:58 +0300

    Simple_Fun_166_Best_Match Done

---
+++ Solutions/5kyu/Simple Fun 166 Best Match.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Solutions._5kyu
+{
+    public class Simple_Fun_166_Best_Match
+    {
+        public int BestMatch(int[] goals1, int[] goals2)
+        {
+            int[] difference = new int[goals1.Length];
+            for (int i = 0; i < goals1.Length; i++)
+            {
+                difference[i] = goals1[i] - goals2[i];
+            }
+
+            int min = 0;
+            for (int i = 0; i < difference.Length; i++)
+            {
+                if (difference[i] < difference[min] || (difference[i] == difference[min] && goals2[i] > goals2[min]))
+                {
+                    min = i;
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Tests/5kyu/Simple Fun 166 Best Match_Test.cs
@@ -0,0 +1,25 @@
+﻿
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class Simple_Fun_166_Best_Match_Test
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new Simple_Fun_166_Best_Match();
+
+            Assert.AreEqual(1, kata.BestMatch(new int[] { 6, 4 }, new int[] { 1, 2 }));
+
+            Assert.AreEqual(0, kata.BestMatch(new int[] { 1 }, new int[] { 0 }));
+
+            Assert.AreEqual(4, kata.BestMatch(new int[] { 1, 2, 3, 4, 5 }, new int[] { 0, 1, 2, 3, 4 }));
+
+            Assert.AreEqual(2, kata.BestMatch(new int[] { 3, 4, 3 }, new int[] { 1, 1, 2 }));
+
+            Assert.AreEqual(1, kata.BestMatch(new int[] { 4, 3, 4 }, new int[] { 1, 1, 1 }));
+
+        }
+    }
+}
