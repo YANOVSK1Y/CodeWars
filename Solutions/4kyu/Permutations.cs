2255083dfb7180cb97c7465e46b75dea25fed511
Andrey <andreyianovskyi78@gmail.com>
2022-09-16 21:07:34 +0300

    Permutations Done

---
+++ Solutions/4kyu/Permutations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._4kyu
+{
+    public class Permutations
+    {
+        public static List<string> SinglePermutations(string s)
+        {
+            List<string> returnstrings = new List<string>();
+            if (s.Length == 1)
+            {
+                returnstrings.Add(s);
+            }
+            else
+            {
+                for (int x = 0; x < s.Length; x++)
+                {
+                    returnstrings.AddRange(SinglePermutations(s.Remove(x, 1)).Select(z => s[x] + z));
+                }
+            }
+
+            return returnstrings.Distinct().ToList();
+        }
+    }
+}
---
+++ Tests/4kyu/Permutations_Test.cs
@@ -0,0 +1,25 @@
+﻿using Solutions._4kyu;
+
+namespace Tests._4kyu
+{
+    public class Permutations_Test
+    {
+        [Test]
+        public void Example1()
+        {
+            Assert.AreEqual(new List<string> { "a" }, Permutations.SinglePermutations("a").OrderBy(x => x).ToList());
+        }
+
+        [Test]
+        public void Example2()
+        {
+            Assert.AreEqual(new List<string> { "ab", "ba" }, Permutations.SinglePermutations("ab").OrderBy(x => x).ToList());
+        }
+
+        [Test]
+        public void Example3()
+        {
+            Assert.AreEqual(new List<string> { "aabb", "abab", "abba", "baab", "baba", "bbaa" }, Permutations.SinglePermutations("aabb").OrderBy(x => x).ToList());
+        }
+    }
+}
