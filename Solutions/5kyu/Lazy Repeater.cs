d3b541cbb20ad1ef8bece6556cfcf7a403916a3b
Andrey <andreyianovskyi78@gmail.com>
2022-09-20 19:49:44 +0300

    Lazy_Repeater Done.

---
+++ Solutions/5kyu/Lazy Repeater.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class Lazy_Repeater
+    {
+        public static Func<char> MakeLooper(string str)
+        {
+            int idx = 0;
+
+            return () => str[idx++ % str.Length];
+        }
+    }
+}
---
+++ Tests/5kyu/Lazy Repeater_Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class Lazy_Repeater_Test
+    {
+
+        [TestFixture]
+        public class Sample_Tests
+        {
+            [Test]
+            public void SampleTest()
+            {
+                Func<char> abc = Lazy_Repeater.MakeLooper("abc");
+                // Two iterations of looper
+                // 1
+                Assert.AreEqual('a', abc());
+                Assert.AreEqual('b', abc());
+                Assert.AreEqual('c', abc());
+                // 2
+                Assert.AreEqual('a', abc());
+                Assert.AreEqual('b', abc());
+                Assert.AreEqual('c', abc());
+            }
+        }
+    }
+}
