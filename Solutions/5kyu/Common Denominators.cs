63a1460c7971f648e04bde5cb67b673102841cc8
Andrey <andreyianovskyi78@gmail.com>
2022-09-24 14:03:20 +0300

    Common_Denominators Done.

---
+++ Solutions/5kyu/Common Denominators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class Common_Denominators
+    {
+        public static string convertFrac(long[,] lst)
+        {
+            var indices = Enumerable.Range(0, lst.GetLength(0));
+            var d = indices.Aggregate(1L, (a, i) => a * lst[i, 1] / gcd(a, lst[i, 1]));
+            return string.Concat(indices.Select(i => $"({d * lst[i, 0] / lst[i, 1]},{d})"));
+        }
+
+        private static long gcd(long a, long b) => b == 0 ? a : gcd(b, a % b);
+    }
+}
---
+++ Tests/5kyu/Common Denominators_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class Common_Denominators_Test
+    {
+        [Test]
+        public void Test1()
+        {
+
+            long[,] lst = new long[,] { { 1, 2 }, { 1, 3 }, { 1, 4 } };
+            Assert.AreEqual("(6,12)(4,12)(3,12)", Common_Denominators.convertFrac(lst));
+
+        }
+    }
+}
