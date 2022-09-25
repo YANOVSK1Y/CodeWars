859a9ad53b7e6995f6e4444f5240df2f08b3da79
Andrey <andreyianovskyi78@gmail.com>
2022-09-25 09:52:34 +0300

    Simple_Pig_Latin Done.

---
+++ Solutions/5kyu/Simple Pig Latin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._5kyu
+{
+    public class Simple_Pig_Latin
+    {
+        public static string PigIt(string str)
+        {
+            return string.Join(" ", str.Split(" ").Select(s => Char.IsLetter(s[0]) ? s.Substring(1, s.Length - 1) + s[0] + "ay" : s));
+        }
+    }
+}
---
+++ Tests/5kyu/Simple Pig Latin_Test.cs
@@ -0,0 +1,14 @@
+﻿using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class Simple_Pig_Latin_Test
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual("igPay atinlay siay oolcay", Simple_Pig_Latin.PigIt("Pig latin is cool"));
+            Assert.AreEqual("hisTay siay ymay tringsay", Simple_Pig_Latin.PigIt("This is my string"));
+        }
+    }
+}
