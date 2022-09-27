0cd67254dbfe2a269af763089e42b884b0c98444
Andrey <andreyianovskyi78@gmail.com>
2022-09-27 10:18:26 +0300

    Did_you_mean Done.

---
+++ Solutions/5kyu/Did you mean.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Solutions._5kyu
+{
+    public class Did_you_mean
+    {
+        private IEnumerable<string> words;
+
+        public Did_you_mean(IEnumerable<string> words)
+        {
+            this.words = words;
+        }
+
+        public string FindMostSimilar(string term)
+        {
+            var scores = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                var (a, b) = term.Length > word.Length ? (term, word) : (word, term);
+                int dl = a.Length - b.Length;
+                int diff = int.MaxValue;
+                for (int i = 0; i <= dl; i++)
+                {
+                    string s = a.Substring(i, b.Length);
+                    int d = s.Zip(b).Where(t => t.First != t.Second).Count();
+                    diff = d < diff ? d : diff;
+                }
+                scores[word] = diff + dl;
+            }
+            return scores.OrderBy(kv => kv.Value).First().Key;
+        }
+    }
+}
---
+++ Tests/5kyu/Did you mean_Test.cs
@@ -0,0 +1,24 @@
+﻿
+using Solutions._5kyu; 
+
+namespace Tests._5kyu
+{
+    public class Did_you_mean_Test
+    {
+        [Test]
+        public void TestDictionary1()
+        {
+            Did_you_mean kata = new Did_you_mean(new List<string> { "cherry", "pineapple", "melon", "strawberry", "raspberry" });
+            Assert.AreEqual("strawberry", kata.FindMostSimilar("strawbery"));
+            Assert.AreEqual("cherry", kata.FindMostSimilar("berry"));
+        }
+
+        [Test]
+        public void TestDictionary2()
+        {
+            Did_you_mean kata = new Did_you_mean(new List<string> { "javascript", "java", "ruby", "php", "python", "coffeescript" });
+            Assert.AreEqual("java", kata.FindMostSimilar("heaven"));
+            Assert.AreEqual("javascript", kata.FindMostSimilar("javascript"));
+        }
+    }
+}
