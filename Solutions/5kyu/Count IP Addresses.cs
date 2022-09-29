cac3db87f1298a8bb25cdcb99fc3772c1ef11263
Andrey <andreyianovskyi78@gmail.com>
2022-09-29 14:40:39 +0300

    Count_IP_Addresses Done

---
+++ Solutions/5kyu/Count IP Addresses.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Solutions._5kyu
+{
+    public class Count_IP_Addresses
+    {
+        public static long IpsBetween(string start, string end)
+        {
+            return IpToNumber(end) - IpToNumber(start);
+        }
+
+        public static long IpToNumber(string ip)
+        {
+            var ints = ip.Split('.').Select(long.Parse).Reverse().ToArray();
+            long value = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                value += ints[i] * (long)Math.Pow(256, i);
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Tests/5kyu/Count IP Addresses_Test.cs
@@ -0,0 +1,16 @@
+﻿
+using Solutions._5kyu;
+
+namespace Tests._5kyu
+{
+    public class Count_IP_Addresses_Test
+    {
+        [Test]
+        public void SmapleTest()
+        {
+            Assert.AreEqual(50, Count_IP_Addresses.IpsBetween("10.0.0.0", "10.0.0.50"));
+            Assert.AreEqual(246, Count_IP_Addresses.IpsBetween("20.0.0.10", "20.0.1.0"));
+            Assert.AreEqual((1L << 32) - 1L, Count_IP_Addresses.IpsBetween("0.0.0.0", "255.255.255.255"));
+        }
+    }
+}
